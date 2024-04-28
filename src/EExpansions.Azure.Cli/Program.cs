a92892a839fc681510fd46dc868eae7b9f22c90d
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2024-04-28 20:02:21 +0900

    init

---
+++ src/EExpansions.Azure.Cli/Program.cs
@@ -0,0 +1,7 @@
+﻿using Azure.Identity;
+using Azure.ResourceManager;
+
+var client = new ArmClient(new AzureCliCredential());
+
+var subscription = await client.GetDefaultSubscriptionAsync();
+Console.WriteLine(subscription.Data.DisplayName);
